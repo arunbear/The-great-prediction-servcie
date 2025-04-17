3fbf5470009d4b0c653f7f1c9df2d403ccfe3133
arunp <arunbear@cpan.org>
2025-04-17 17:10:56 +0100

    Add initial project files

---
+++ src/main/java/com/example/GreatPredictionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatPredictionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreatPredictionServiceApplication.class, args);
+    }
+
+}
